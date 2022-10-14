8e00ec927fe0a74aec2060df9829527e698e1f79
Charles Warwick <81972652+cawarwick@users.noreply.github.com>
2022-10-14 15:57:16 -0400

    Update constant.py
    CAW updates with new variables for plotting and some reorganization

--- constant.py
+++ constant.py
@@ -7,20 +7,59 @@ import numpy as np
 ##################################### BELOW ARE THE MOST IMPORTANT VARIABLES TO MODIFY#####################
 
 #set debug to True to show all debugging statements, and set to False to hide all (also saves time)
-debug = True
+debug = False
 #set varyingThreshold to have different threshold for each stimuli for each ROI (e.g. 3 ROI x 5 stimuli = 15 different thresholdds)
-varyingThreshold = True
+varyingThreshold = True ##set to True to skip median determination of thresholds
+# signal above threshold are considered an event
+threshold = 0.4
+##Threshold for SD 
+SD=5
+
+##Set to 1 if you already have smoothed/normalized data and just need to re run thresholds
+ThresholdsOnly=0
+
+
 #folder containing 3 children folder: Data (original data), OutputData(normalized, smoothed, etc. data),
-# and Figure(output figures)
-parentFolder = "#468"
-#to be added to each file name
-planeNumber= "P2"
+parentFolder = "#462"
+
 #Frame per second for the experiment (used only in Plot.py)
-fps = 7.4
+fps = 8.46
+# sample spacing
+T = 1.0 / fps # 8Hz
+
+#window for normalization
+window = 2500 
+#percentile of window for normalization
+percentile=30 ##depending on how active your cells are, anything from the 5th to 30th percentile is usually fine. 
+
+#window size for smoothing
+# this is the window in which the polynomial fits. Larger numbers are more smooth, 
+# but can decrease the height of transient peaks. Choose something on the same scale as your events, 
+# e.g. if a transient peak lasts around 30 frames, 15 is a good starting point.
+window_size = 9
+#polynomial order for smoothing
+#this is the order of polynomial used for smoothing. 
+# Bigger numbers fit the curve more closely (i.e. less smooth, but more accurate)
+polynomial = 3
+
+#Size of graph created by Plot.py
+SecondsPerInch=800 
+##you can go as low as 800 for a compact graph but it'll be hard to read.
+# ~300 the bare minimum for 15s VF spacing, but tight, ~100 would be better. C
+# CICADA is totally readable at 800
+
+
+
+
+#__________________Other variables___________________#
+
+#to be added to each file name
+planeNumber= "P0"
 
 #start of the stimulus (in frames, cannot be 0, must be >=1)
 #input stimStart = "all" if want the entirety of the recording
 stimStart = 1
+
 #if want the entirety of the recording, input stimEnd = "all"
 #if exceed max frame number, automatically take the 
 # stimEnd = 2000*7.4
@@ -62,22 +101,6 @@ prefix = expNumber + "_" + planeNumber+ " _frm" + str(stimStart) + "-"+ str(stim
 #the prefix excluding frame number
 splPrefix = expNumber + "_" + planeNumber+ "_"
 
-# sample spacing
-T = 1.0 / 8.0 # 8Hz
-#window for normalization
-window = 400 
-#window size for smoothing
-# this is the window in which the polynomial fits. Larger numbers are more smooth, 
-# but can decrease the height of transient peaks. Choose something on the same scale as your events, 
-# e.g. if a transient peak lasts around 30 frames, 15 is a good starting point.
-window_size = 15
-##depending on how active your cells are, anything from the 5th to 30th percentile is usually fine. 
-percentile=30 
-#polynomial order for smoothing
-#this is the order of polynomial used for smoothing. 
-# Bigger numbers fit the curve more closely (i.e. less smooth, but more accurate)
-polynomial = 3
+
 #Design a digital low-pass filter at 4Hz to remove frequencies faster than 4Hz
 sos = signal.butter(N = 10,Wn = 4, btype = 'lowpass', fs=8.2, output='sos') 
-# signal above threshold are considered an event
-threshold = 0.5
