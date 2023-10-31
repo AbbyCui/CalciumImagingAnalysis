c7a5d2c1721969757e979c018c9346cf87b9376a
Charles Warwick <81972652+cawarwick@users.noreply.github.com>
2023-10-31 13:04:49 -0400

    bug fixes and QoL add
    Added variables at head and fixed header bug

--- Rename20xAvg.py
+++ Rename20xAvg.py
@@ -7,42 +7,41 @@ import sys
 import os
 
 #enter parameters
-exp = str(465)
-planeRange = range(1,2) #range(1,5) iterates over plane 1,2,3,4
+DataFolder = 'E:/#489 - Copy/#489/OutputData/'
+exp = str(489)
+planeRange = range(0,5) #range(1,5) iterates over plane 1,2,3,4
+Avg_Amount = 5
 
 ####
 ROIsToRemove = np.zeros((1,1))
 for p in planeRange:
     #import and extract smoothed data
-    filename = exp+'_P'+str(p)
-    splPATH = 'D:/DATA/Calcium Oscillation Data/Figures and source data/4880 CICADA response/smoothed/#' + filename
+    filename = '#' + exp + '_P'+str(p)
+    splPATH = DataFolder + filename
     smoothed = np.loadtxt(splPATH +"_Smoothed.csv",delimiter=',',dtype=str)
     f = Utility.extractData(debug, smoothed, ROIsToRemove = ROIsToRemove, stimStart = stimStart, stimEnd = stimEnd)
 
     #calculate 20x average
     y,x = f.shape
-    avg = np.empty(((y-1)//20,x)) #an empty array to stored the 20x results
+    avg = np.empty(((y-1)//Avg_Amount,x)) #an empty array to stored the 20x results
 
-    for i in range((y-1)//20-1):
-        thisf = np.array(f[1+i*20:i*20+20,...],dtype = float)
+    for i in range((y-1)//Avg_Amount-1):
+        thisf = np.array(f[1+i*Avg_Amount:i*Avg_Amount+Avg_Amount,...],dtype = float)
         # print(thisf)
         avg[i+1,...] = np.mean(thisf,axis = 0) #axis =0 -> average downwards across 20 rows
     out = avg.astype(str)
 
     #change ROI name to format such as exp465_P0_ROI002
     head=f[0,...]
-    print('shape=',head.shape)
-    outhead = np.full_like(head,"12345678901234")
+    outhead = np.full_like(head,"12345678901234567", dtype="object")
     for i in range(len(head)):
         thishead = head[i]
         a = thishead.replace('Mean','')
         a = a.zfill(3)
-        # outhead[i] = 'exp'+filename+"_ROI"+a
-        print('outhead before:',outhead[i])
-        outhead[i] = 'x'+filename+"R"+a
-        print('outhead:',outhead[i])
+        thisname = filename+"_ROI"+a
+        outhead[i] = thisname
     out[0,...] = outhead
     print('changed header:',head[1],'to',outhead[1])
-    np.savetxt("D:/DATA/Calcium Oscillation Data/Figures and source data/4880 CICADA response/#"+filename+"ttt_20x avg.csv",out, delimiter=',', comments='', fmt='%s')
+    AvgStr = str(Avg_Amount)
+    np.savetxt(DataFolder + filename  +"_"+ AvgStr + "x avg.csv",out, delimiter=',', comments='', fmt='%s')
 print('finished')
-
