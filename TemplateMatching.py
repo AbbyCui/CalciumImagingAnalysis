9276e90e4320001390b52dcfdd3b0000026e67fd
Charles Warwick <81972652+cawarwick@users.noreply.github.com>
2023-10-31 20:48:15 -0400

    Update TemplateMatching.py

--- TemplateMatching.py
+++ TemplateMatching.py
@@ -7,12 +7,23 @@ import sys
 import os
 
 #enter parameters
-expNum = str(465)
-planeRange = range(1,2)
-dir = 'D:/DATA/Calcium Oscillation Data/Figures and source data/4880 CICADA response/'
-output = 'filtered_noRes' #specify output file name
+Avg_Amount = '5' ##this needs to be a string 
+ParentDirectory = 'E:/#489 - Copy/#489/'
+template_name = "#489_Non-Itch IN"
+expNum = str(489)
+planeRange = range(0,5) #range(1,5) iterates over plane 1,2,3,4
+
+
+#assign directory based on our standard organization
+dir = ParentDirectory + 'OutputData/' ##where your averaged/smoothed traces are
+temp_dir = ParentDirectory + 'Data/' ##where your filter files are
+output = template_name #specify output file name, or by default use the name of the template CSV
 #load files with headers for ROIs to be extracted (e.g. exp456_P1_ROI002)
-template = np.loadtxt("D:/DATA/Calcium Oscillation Data/Figures and source data/4880 CICADA response/ROInames_4880nonresponder.csv",delimiter=',',dtype=str)
+
+template = np.loadtxt(temp_dir + template_name + '.csv', delimiter=',',comments='%',dtype=str)
+
+
+#template = np.loadtxt(ParentDirectory + template_name + ".csv",delimiter=',',dtype=str)
 
 #res=np.loadtxt("D:/DATA/Calcium Oscillation Data/Figures and source data/4880 CICADA response/ROInames_4880responder.csv",delimiter=',',dtype=str)
 numROI = len(template)
@@ -21,8 +32,8 @@ numROI = len(template)
 # import raw data, normalized data, and smoothed data
 for p in planeRange:
     filename = '#'+expNum+'_P'+str(p)
-    splPATH = dir + filename
-    avg = np.loadtxt(splPATH +"_20x avg_test.csv",delimiter=',',dtype=str)
+    splPATH = dir + filename 
+    avg = np.loadtxt(splPATH + "_" + str(Avg_Amount) + "x avg.csv",delimiter=',',comments='%',dtype=str)
     ROIsToRemove = np.zeros((1,1))
     f = Utility.extractData(debug, avg, ROIsToRemove = ROIsToRemove, stimStart = stimStart, stimEnd = stimEnd)
     print('the first 5 ROIs in f:',f[0,0:5])
@@ -43,4 +54,4 @@ for p in planeRange:
     
 filtered = np.vstack((template[None,...],filtered))
 filtered = np.hstack((f[...,0][...,None],filtered))
-np.savetxt("D:/DATA/Calcium Oscillation Data/Figures and source data/4880 CICADA response/"+output+".csv",filtered, delimiter=',', comments='', fmt='%s')
+np.savetxt(dir+output+".csv",filtered, delimiter=',', comments='', fmt='%s')
