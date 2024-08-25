985b5f78ceaff3cf47671addccd812c9452f87bd
Charles Warwick <81972652+cawarwick@users.noreply.github.com>
2024-08-25 15:21:45 -0400

    Update 05Plot.py
    bug fix

--- 05Plot.py
+++ 05Plot.py
@@ -1,25 +1,33 @@
+##Variables you need to change
+
+SecondsPerInch=50 ##10-30s spaced stimul are easily visible in the 25-100 range (25 SPI is best for 10s stimuli, but 50 is readable), pharmacology is fine at 300. Most stimuli are unreadable beyond 200.
+start= 1 ##frame at which to start graphing the data
+stop= 20000  ##frame at which to stop the graphing
+
+##-------------------------------##
+
 import multiprocessing
 import subprocess
 
-SecondsPerInch=100
-start=1
-stop="all"
+start=str(start) ##These need to be strings for some reason
+stop=str(stop) ##These need to be strings for some reason
+SecondsPerInch=str(SecondsPerInch)
 
 def run_script(script_args):
     """Function to run a script using subprocess with arguments"""
-    script_name, args = script_args
+    script_name, args1, args2, args3, arg4 = script_args
     # Construct the command with the script and arguments
-    command = ['python', script_name] + args
+    command = ['python', script_name] +args1 + args2 + args3 + arg4
     subprocess.run(command, check=True)
 
 def main():
     # List of tuples where each tuple contains a script name and a list of arguments
     scripts_with_args = [
-        ('Plot.py', ['P0',start, stop, SecondsPerInch]),
-        ('Plot.py', ['P1',start, stop, SecondsPerInch]),
-        ('Plot.py', ['P2',start, stop, SecondsPerInch]),
-        ('Plot.py', ['P3',start, stop, SecondsPerInch]),
-        ('Plot.py', ['P4',start, stop, SecondsPerInch]),
+        ('Plot.py', ['P0'],[start],[stop],[SecondsPerInch]),
+        ('Plot.py', ['P1'],[start],[stop],[SecondsPerInch]),
+        ('Plot.py', ['P2'],[start],[stop],[SecondsPerInch]),
+        ('Plot.py', ['P3'],[start],[stop],[SecondsPerInch]),
+        ('Plot.py', ['P4'],[start],[stop],[SecondsPerInch]),
     ]
     # Create a pool of processes, each running one of the scripts
     with multiprocessing.Pool(processes=len(scripts_with_args)) as pool:
